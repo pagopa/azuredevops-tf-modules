bdec879c8b3cc65426ed8e44632287a956646497
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-01-09 18:31:14 +0100

    init pipeline

---
+++ azuredevops_build_definition_generic/main.tf
@@ -0,0 +1,111 @@
+terraform {
+  required_version = ">= 0.14.5"
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.8"
+    }
+  }
+}
+
+resource "azuredevops_build_definition" "pipeline" {
+  project_id      = var.project_id
+  name            = "${var.pipeline_name}"
+  path            = "\\${var.repository.name}"
+  agent_pool_name = var.agent_pool_name
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.repository.organization}/${var.repository.name}"
+    branch_name           = var.repository.branch_name
+    yml_path              = "${var.repository.pipelines_path}/${var.pipeline_yml_filename}"
+    service_connection_id = var.github_service_connection_id
+  }
+
+  # ci_trigger {
+  #   use_yaml = var.ci_trigger_use_yaml == false ? null : true
+  # }
+
+  dynamic "ci_trigger" {
+    for_each = var.ci_trigger_use_yaml == false ? [] : ["dummy"]
+
+    content {
+      use_yaml = var.ci_trigger_use_yaml
+    }
+  }
+
+  # todo not works
+  # dynamic "ci_trigger" {
+  #   for_each = var.ci_trigger
+
+  #   content {
+  #     override {
+  #       batch                            = false
+  #       max_concurrent_builds_per_branch = 1
+  #       polling_interval                 = 0
+  #       branch_filter {
+  #         include = ci_trigger.value.branch_filter.include
+  #         exclude = ci_trigger.value.branch_filter.exclude
+  #       }
+  #       path_filter {
+  #         include = ci_trigger.value.path_filter.include
+  #         exclude = ci_trigger.value.path_filter.exclude
+  #       }
+  #     }
+  #   }
+  # }
+
+  dynamic "variable" {
+    for_each = var.variables
+    iterator = variable
+
+    content {
+      name           = upper(variable.key)
+      value          = variable.value
+      allow_override = false
+    }
+  }
+
+  dynamic "variable" {
+    for_each = var.variables_secret
+    iterator = variable_secret
+
+    content {
+      name           = upper(variable_secret.key)
+      secret_value   = variable_secret.value
+      is_secret      = true
+      allow_override = false
+    }
+  }
+}
+
+# This is to work around an issue with azuredevops_resource_authorization
+# The service connection resource is not ready immediately
+# so the recommendation is to wait 30 seconds until it's ready
+# https://github.com/microsoft/terraform-provider-azuredevops/issues/266
+resource "time_sleep" "wait" {
+  create_duration = "30s"
+}
+
+# github_service_connection_id serviceendpoint authorization
+resource "azuredevops_resource_authorization" "github_service_connection_authorization" {
+  depends_on = [azuredevops_build_definition.pipeline, time_sleep.wait]
+
+  project_id    = var.project_id
+  resource_id   = var.github_service_connection_id
+  definition_id = azuredevops_build_definition.pipeline.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# others service_connection_ids serviceendpoint authorization
+resource "azuredevops_resource_authorization" "service_connection_ids_authorization" {
+  depends_on = [azuredevops_build_definition.pipeline, time_sleep.wait]
+  count      = var.service_connection_ids_authorization == null ? 0 : length(var.service_connection_ids_authorization)
+
+  project_id    = var.project_id
+  resource_id   = var.service_connection_ids_authorization[count.index]
+  definition_id = azuredevops_build_definition.pipeline.id
+  authorized    = true
+  type          = "endpoint"
+}
---
+++ azuredevops_build_definition_generic/variables.tf
@@ -0,0 +1,76 @@
+variable "project_id" {
+  type        = string
+  description = "(Required) Azure DevOps project ID"
+}
+
+variable "repository" {
+  type = object({
+    organization    = string
+    name            = string
+    branch_name     = string
+    pipelines_path  = string
+    yml_filename = string
+  })
+  description = "(Required) GitHub repository attributes"
+}
+
+variable "ci_trigger_use_yaml" {
+  type        = bool
+  description = "(Optional) Use the azure-pipeline file for the build configuration. Defaults to false."
+  default     = false
+}
+
+# todo not works
+# variable "ci_trigger" {
+#   type = object({
+#     branch_filter = object({
+#       exclude = list(string)
+#       include = list(string)
+#     })
+#     path_filter = object({
+#       exclude = list(string)
+#       include = list(string)
+#     })
+#   })
+#   description = "(Optional) CI trigger policy"
+#   default     = null
+# }
+
+variable "github_service_connection_id" {
+  type        = string
+  description = "(Required) GitHub service connection ID used to link Azure DevOps."
+}
+
+variable "variables" {
+  type        = map(any)
+  default     = null
+  description = "(Optional) Pipeline variables"
+}
+
+variable "variables_secret" {
+  type        = map(any)
+  default     = null
+  description = "(Optional) Pipeline secret variables"
+}
+
+variable "service_connection_ids_authorization" {
+  type        = list(string)
+  default     = null
+  description = "(Optional) List service connection IDs that pipeline needs authorization. github_service_connection_id is authorized by default"
+}
+
+variable "agent_pool_name" {
+  type        = string
+  default     = "Hosted Ubuntu 1604"
+  description = "The agent pool that should execute the build"
+}
+
+variable "pipeline_yml_filename" {
+  type        = string
+  description = "(Required) Yaml pipeline filename"
+}
+
+variable "pipeline_name" {
+  type        = string
+  description = "(Required) Pipeline name"
+}
