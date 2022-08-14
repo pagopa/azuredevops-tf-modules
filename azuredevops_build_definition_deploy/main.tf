81c7e57c1895e95feab5fb66ce7c8d7fc898dcee
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-14 15:26:11 +0200

    try deploy fix

--- azuredevops_build_definition_deploy/main.tf
+++ azuredevops_build_definition_deploy/main.tf
@@ -24,29 +24,35 @@ resource "azuredevops_build_definition" "pipeline" {
     }
   }
 
-  pull_request_trigger {
-    use_yaml       = var.pull_request_trigger_use_yaml == false ? null : true
-    initial_branch = var.repository.branch_name
+  dynamic "pull_request_trigger" {
 
-    forks {
-      enabled       = false
-      share_secrets = false
-    }
+    for_each = var.pull_request_trigger_use_yaml == false ? [] : ["dummy"]
 
-    dynamic "override" {
-      for_each = var.pull_request_trigger_use_yaml == true ? [] : ["dummy"]
+    content {
+      use_yaml       = var.pull_request_trigger_use_yaml == false ? null : true
+      initial_branch = var.repository.branch_name
 
-      content {
-        auto_cancel = false
-        branch_filter {
-          include = [var.repository.branch_name]
-        }
-        path_filter {
-          exclude = []
-          include = []
-        }
+      forks {
+        enabled       = false
+        share_secrets = false
       }
+
+      dynamic "override" {
+        for_each = var.pull_request_trigger_use_yaml == true ? [] : ["dummy"]
+
+        content {
+          auto_cancel = false
+          branch_filter {
+            include = [var.repository.branch_name]
+          }
+          path_filter {
+            exclude = []
+            include = []
+          }
+        }
+      }  
     }
+
   }
 
   # todo not works
