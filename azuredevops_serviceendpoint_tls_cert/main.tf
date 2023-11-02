f79f5f71852a3c89b9bed84118c93da0cc7d1fab
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-11-02 19:41:41 +0100

    added prototype of managed identity

--- azuredevops_serviceendpoint_tls_cert/main.tf
+++ azuredevops_serviceendpoint_tls_cert/main.tf
@@ -1,95 +1,27 @@
-# This is to work around an issue with azuredevops_resource_authorization
-# The service connection resource is not ready immediately
-# so the recommendation is to wait 30 seconds until it's ready
-resource "time_sleep" "wait" {
-  create_duration = "60s"
+resource "azurerm_user_assigned_identity" "identity" {
+  location            = var.location
+  name                = var.name
+  resource_group_name = var.resource_group_name
 }
 
-resource "null_resource" "this" {
-  # needs az cli > 2.0.81
-  # see https://github.com/Azure/azure-cli/issues/12152
-
-  triggers = {
-    renew_token                      = var.renew_token
-    name                             = var.name
-    subscription_name                = var.subscription_name
-    subscription_id                  = var.subscription_id
-    credential_subcription           = var.credential_subcription
-    credential_key_vault_name        = var.credential_key_vault_name
-    default_roleassignment_rg_prefix = var.default_roleassignment_rg_prefix
+resource "azuredevops_serviceendpoint_azurerm" "azurerm" {
+  project_id                    = var.project_id
+  service_endpoint_name         = "service_connection_${var.name}"
+  description                   = "Managed by Terraform"
+  service_endpoint_authentication_scheme = "WorkloadIdentityFederation"
+  credentials {
+    serviceprincipalid  = azurerm_user_assigned_identity.identity.client_id
   }
-
-  # https://docs.microsoft.com/it-it/cli/azure/ad/sp?view=azure-cli-latest#az_ad_sp_create_for_rbac
-  provisioner "local-exec" {
-    command = <<EOT
-      SP_CREDENTIAL_VALUES=$(az ad sp create-for-rbac \
-        --name "azdo-sp-${self.triggers.name}" \
-        --role "Reader" \
-        --scope "/subscriptions/${self.triggers.subscription_id}/resourceGroups/${self.triggers.default_roleassignment_rg_prefix}default-roleassignment-rg" \
-        -o json)
-
-      az keyvault secret set \
-        --subscription "${self.triggers.credential_subcription}" \
-        --vault-name "${self.triggers.credential_key_vault_name}" \
-        --name "azdo-sp-${self.triggers.name}" \
-        --value "$SP_CREDENTIAL_VALUES"
-    EOT
-  }
-
-  # https://docs.microsoft.com/it-it/cli/azure/ad/sp?view=azure-cli-latest#az_ad_sp_delete
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      SERVICE_PRINCIPAL_ID=$(az keyvault secret show \
-        --subscription "${self.triggers.credential_subcription}" \
-        --vault-name "${self.triggers.credential_key_vault_name}" \
-        --name "azdo-sp-${self.triggers.name}" \
-        -o tsv --query value | jq -r '.appId')
-
-      az ad sp delete --id "$SERVICE_PRINCIPAL_ID"
-
-      az keyvault secret set \
-        --subscription "${self.triggers.credential_subcription}" \
-        --vault-name "${self.triggers.credential_key_vault_name}" \
-        --name "azdo-sp-${self.triggers.name}" \
-        --value "DELETEME" \
-        --disabled true \
-        --description "DELETEME"
-    EOT
-  }
-}
-
-module "secrets" {
-  depends_on = [null_resource.this]
-  source     = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v6.15.2"
-
-  resource_group = var.credential_key_vault_resource_group
-  key_vault_name = var.credential_key_vault_name
-
-  secrets = [
-    "azdo-sp-${var.name}",
-  ]
-}
-
-# Azure DevOps service connection
-resource "azuredevops_serviceendpoint_azurerm" "this" {
-  depends_on = [null_resource.this, module.secrets]
-
-  project_id            = var.project_id
-  service_endpoint_name = "${upper(var.name)}-SERVICE-CONN"
-  description           = "${upper(var.name)} Service connection for TLS certificates"
-
-  azurerm_subscription_name = var.subscription_name
   azurerm_spn_tenantid      = var.tenant_id
   azurerm_subscription_id   = var.subscription_id
-
-  credentials {
-    serviceprincipalid  = jsondecode(module.secrets.values["azdo-sp-${var.name}"].value).appId
-    serviceprincipalkey = jsondecode(module.secrets.values["azdo-sp-${var.name}"].value).password
-  }
+  azurerm_subscription_name = var.subscription_name
 }
 
-data "azuread_service_principal" "this" {
-  depends_on   = [time_sleep.wait]
-  display_name = jsondecode(module.secrets.values["azdo-sp-${var.name}"].value).displayName
+resource "azurerm_federated_identity_credential" "federated_setup" {
+  parent_id           = azurerm_user_assigned_identity.identity.id
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  audience            = [local.default_audience_name]
+  issuer              = azuredevops_serviceendpoint_azurerm.azurerm.workload_identity_federation_issuer
+  subject             = azuredevops_serviceendpoint_azurerm.azurerm.workload_identity_federation_subject
 }
--- azuredevops_serviceendpoint_tls_cert/variables.tf
+++ azuredevops_serviceendpoint_tls_cert/variables.tf
@@ -1,21 +1,24 @@
+locals {
+  default_audience_name = "api://AzureADTokenExchange"
+}
+
+variable "location" {
+  type = string
+}
+
 variable "name" {
   type        = string
   description = "(Required) Service principal name"
 }
 
+variable "resource_group_name" {
+  type = string
+  description = "Resource group where the managed identity will be create"
+}
+
 variable "project_id" {
   type        = string
   description = "(Required) Azure DevOps project ID"
-}
-
-variable "renew_token" {
-  type        = string
-  description = "(Required) Renew token to recreate service principal. Change it to renew service principal credentials"
-}
-
-variable "subscription_name" {
-  type        = string
-  description = "(Required) Azure Subscription name related to tenant where create service principal"
 }
 
 variable "subscription_id" {
@@ -28,23 +31,36 @@ variable "tenant_id" {
   description = "(Required) Azure Tenant ID related to tenant where create service principal"
 }
 
-variable "credential_subcription" {
+variable "subscription_name" {
   type        = string
-  description = "(Required) Azure Subscription where store service principal credentials"
+  description = "(Required) Azure Subscription name related to tenant where create service principal"
 }
 
-variable "credential_key_vault_name" {
-  type        = string
-  description = "(Required) Key vault name where store service principal credentials"
-}
+# variable "renew_token" {
+#   type        = string
+#   description = "(Required) Renew token to recreate service principal. Change it to renew service principal credentials"
+# }
 
-variable "credential_key_vault_resource_group" {
-  type        = string
-  description = "(Required) Key vault resource group where store service principal credentials"
-}
 
-variable "default_roleassignment_rg_prefix" {
-  type        = string
-  default     = ""
-  description = "(Optional) Add a prefix to default_roleassignment_rg"
-}
+
+
+# variable "credential_subcription" {
+#   type        = string
+#   description = "(Required) Azure Subscription where store service principal credentials"
+# }
+
+# variable "credential_key_vault_name" {
+#   type        = string
+#   description = "(Required) Key vault name where store service principal credentials"
+# }
+
+# variable "credential_key_vault_resource_group" {
+#   type        = string
+#   description = "(Required) Key vault resource group where store service principal credentials"
+# }
+
+# variable "default_roleassignment_rg_prefix" {
+#   type        = string
+#   default     = ""
+#   description = "(Optional) Add a prefix to default_roleassignment_rg"
+# }
