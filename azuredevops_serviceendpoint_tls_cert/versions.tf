ac7d93a38d1049fa370ff3aa7f85c0b1125f98d0
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-11-02 15:24:38 +0100

    added service endpoint for tls cert

---
+++ azuredevops_serviceendpoint_tls_cert/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.10.0"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">= 0.5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.71.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.7.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 1.3.0"
+    }
+  }
+}
+
