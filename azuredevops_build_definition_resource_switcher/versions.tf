2ccfd8d77370cbc482510d3f9a36894aa15e305f
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-26 14:52:51 +0100

    upgrade azurerm check

--- azuredevops_build_definition_resource_switcher/versions.tf
+++ azuredevops_build_definition_resource_switcher/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.71.0"
+      version = "<= 3.90.0"
     }
     null = {
       source  = "hashicorp/null"
--- azuredevops_serviceendpoint_azurerm_plan/versions.tf
+++ azuredevops_serviceendpoint_azurerm_plan/versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.71.0"
+      version = "<= 3.90.0"
     }
     time = {
       source  = "hashicorp/time"
