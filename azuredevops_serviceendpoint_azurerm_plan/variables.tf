afe3b95a977f02db815e692ed96cdce0e445ac93
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-09-09 15:45:59 +0200

    added "App Compliance Automation Reader", to plan module

--- azuredevops_serviceendpoint_azurerm_plan/variables.tf
+++ azuredevops_serviceendpoint_azurerm_plan/variables.tf
@@ -14,6 +14,7 @@ locals {
       "Key Vault Secrets User",
       "DocumentDB Account Contributor",
       "API Management Service Contributor",
+      "App Compliance Automation Reader",
     ]
   }
 }
