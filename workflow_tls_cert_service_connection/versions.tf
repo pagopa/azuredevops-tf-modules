3ccc88a7613c904f9c76428ff341e50c551456ea
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-22 11:57:12 +0200

    fix: add versions.tf to workflow_tls_cert_service_connection

---
+++ workflow_tls_cert_service_connection/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 1.8"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.115.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
+}
