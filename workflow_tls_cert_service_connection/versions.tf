2e3b3d1e6616d9485bc6356d87b3319da3895fc2
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2025-04-22 12:04:27 +0200

    fix: Force release (#92)
    * fix: updated versions.tf
    
    * fix: updated versions.tf
    
    * fix: updated versions.tf

--- workflow_tls_cert_service_connection/versions.tf
+++ workflow_tls_cert_service_connection/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.115.1"
+      version = "~> 3.107"
     }
     null = {
       source  = "hashicorp/null"
